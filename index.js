2542720b5eb125ad2403da05d80a0a15b8f84519
Jeremy Parker <jrobinparker@gmail.com>
2018-11-20 20:33:02 +0000

    Completed lesson

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much.";
+const token = '3de496963748ffea575eb3c83e558fe2a8d0a664'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
